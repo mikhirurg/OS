de3c68ad60e1d3dab30128ec0d5944b5a2703110
Mikhail Ushakov <mikhail.a.ushakov@gmail.com>
2020-09-05 20:42:29 +0300

    Task 10 fix

---
+++ LABS/LAB1/task10/wordstat.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Top 3 common words in \"man bash\" (length >= 4): "
+man bash | \
+sed 's/[\ \n\t]\+/\n/g'| \
+awk '{ if (length($0) >= 4) print }' |  \
+awk '{ 
+     stat[$0]++
+     }
+     END { 
+     for (var in stat) 
+         print var, stat[var]
+     }' | \
+sort -k2 -gr | \
+head -n3
