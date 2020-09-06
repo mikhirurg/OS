c808f6f7b03c73fd711b731aa540b315a71594a1
Mikhail Ushakov <mikhail.a.ushakov@gmail.com>
2020-09-06 14:47:35 +0300

    task6 fix

--- LABS/LAB1/task6/grab_warn_info.bash
+++ LABS/LAB1/task6/grab_warn_info.bash
@@ -3,3 +3,4 @@ grep -E '\[( )*[0-9]*.[0-9]*\] \((II|WW)\)' /var/log/anaconda/X.log | \
 sed 's/(II)/Information:/' | \
 sed 's/(WW)/Warning:/' | \
 sort -k3 -r > full.log
+awk '{ print $0 }' full.log
